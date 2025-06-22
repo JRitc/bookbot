05fbaa92e4fda54d780a7b64bce69011803c4bad
JRitc <Jake.Ritchie7@gmail.com>
2025-06-22 12:21:26 +0100

    added arguments to allow for book input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count
 from stats import get_char_count
 from stats import sort_on
@@ -10,14 +11,18 @@ def get_book_text(filepath):
 
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     char_count = get_char_count(text)
     sorted_char_list = dict_to_sorted_list(char_count)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
